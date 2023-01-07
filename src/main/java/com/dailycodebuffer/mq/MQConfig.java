42865ccaa4400adb3c8df7a7afb7d5c35a0d540b
acpuma <cumacakmak@gmail.com>
2023-01-07 23:52:10 +0300

    First Commit

---
+++ src/main/java/com/dailycodebuffer/mq/MQConfig.java
@@ -0,0 +1,34 @@
+package com.dailycodebuffer.mq;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MQConfig {
+
+    public static final String MESSAGE_QUEUE = "message_queue";
+    public static final String MESSAGE_EXCHANGE = "message_exchange";
+    public static final String MESSAGE_ROUTING_KEY = "message_routingKey";
+
+    @Bean
+    public Queue queue() {
+        return new Queue(MESSAGE_QUEUE);
+    }
+
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange(MESSAGE_EXCHANGE);
+    }
+
+    @Bean
+    public Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder
+                .bind(queue)
+                .to(exchange)
+                .with(MESSAGE_ROUTING_KEY);
+    }
+}
