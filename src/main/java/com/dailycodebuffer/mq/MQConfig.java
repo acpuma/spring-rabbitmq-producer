dfc5e81a37f470b6a331d89adcf0f1124df25141
acpuma <cumacakmak@gmail.com>
2023-01-08 00:17:04 +0300

    Add rabbitmq message publisher and listener

--- src/main/java/com/dailycodebuffer/mq/MQConfig.java
+++ src/main/java/com/dailycodebuffer/mq/MQConfig.java
@@ -1,27 +1,28 @@
 package com.dailycodebuffer.mq;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MQConfig {
 
-    public static final String MESSAGE_QUEUE = "message_queue";
-    public static final String MESSAGE_EXCHANGE = "message_exchange";
-    public static final String MESSAGE_ROUTING_KEY = "message_routingKey";
+    public static final String QUEUE = "message_queue";
+    public static final String EXCHANGE = "message_exchange";
+    public static final String ROUTING_KEY = "message_routingKey";
 
     @Bean
     public Queue queue() {
-        return new Queue(MESSAGE_QUEUE);
+        return new Queue(QUEUE);
     }
 
     @Bean
     public TopicExchange exchange() {
-        return new TopicExchange(MESSAGE_EXCHANGE);
+        return new TopicExchange(EXCHANGE);
     }
 
     @Bean
@@ -29,6 +30,18 @@ public class MQConfig {
         return BindingBuilder
                 .bind(queue)
                 .to(exchange)
-                .with(MESSAGE_ROUTING_KEY);
+                .with(ROUTING_KEY);
+    }
+
+    @Bean
+    public MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public AmqpTemplate template(ConnectionFactory connectionFactory) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(messageConverter());
+        return template;
     }
 }
